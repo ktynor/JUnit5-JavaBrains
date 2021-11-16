75b8743535a8eac8238142fcc59c8eab32e7dfa3
ktynor <kt9@tlen.pl>
2021-11-16 17:26:45 +0000

    shouldComputeCircleArea() Test Driven Development, first implement test method and than actual method to be tested.

--- src/main/java/mathutils/MathUtils.java
+++ src/main/java/mathutils/MathUtils.java
@@ -1,7 +1,11 @@
 package mathutils;
 
 public class MathUtils {
-    public int add(int a, int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double computeCircleArea(double radius) {
+        return Math.PI * radius * radius;
     }
 }
--- src/test/java/mathutils/MathUtilsTest.java
+++ src/test/java/mathutils/MathUtilsTest.java
@@ -7,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MathUtilsTest {
 
     @Test
-    void ShouldAddTwoNumbers() {
+    void shouldAddTwoNumbers() {
         MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldComputeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(Math.PI * 100, mathUtils.computeCircleArea(10),"Should return circle area");
     }
 }
